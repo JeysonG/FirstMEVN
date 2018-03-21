4629d51b21940ffb39ab1b8c75cc851b26af2f33
Jeyson Gonzalez <jjjeysoon8@outlook.es>
2018-03-21 10:17:37 -0700

    Configurando Vuejs

--- src/app/main.js
+++ src/app/main.js
@@ -1,1 +1,16 @@
-alert('Vuejs Funcionando');
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+Vue.use(VueAxios, axios);
+
+import App from './App.vue';
+
+const routes = [
+
+];
+
+const router = new VueRouter({mode: 'history'}, {routes: routes});
+new Vue(Vue.util.extend({router}, App)).$mount('#app');
