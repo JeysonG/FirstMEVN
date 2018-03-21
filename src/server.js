f8942363741043f4ce3826b7a204040709074b00
Jeyson Gonzalez <jjjeysoon8@outlook.es>
2018-03-21 08:57:19 -0700

    Se modifico src/server.js en configuracion del servidor y movimiento a los directorios de rutas y modelos

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
+const path = require('path');
 const bodyparser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 const app = express();
+
+const itemRoute = require('./routes/item');
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
@@ -8,9 +12,21 @@ mongoose.connect('mongodb://localhost/muscleapp')
 .then(() => console.log('db connected'))
 .catch(err => console.log(err));
 
+//settings
+
 app.set('port', process.env.PORT || 3000);
 
+//middlewres
+app.use(cors());
 app.use(bodyparser.json());
+
+//routes
+
+app.use('/item', itemRoute);
+
+//static files
+
+app.use(express(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
     console.log('Server on port', 30000);
